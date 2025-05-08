6aa226a0d67b3caeff8a0f3798ddacb5ee35287f
sjmy <23250749+sjmy@users.noreply.github.com>
2025-05-08 10:11:37 -0700

    Completed exercise 6

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result = "";
 
+    if (num < 0) {
+        result = "ERROR";
+    };
+
+    for (n = 0; n < num; n++) {
+        result += string;
+    };
+
+    return result;
 };
 
 // Do not edit below this line
