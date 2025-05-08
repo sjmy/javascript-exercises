05de2a3d7141915f33749d4c9263c63795143650
sjmy <23250749+sjmy@users.noreply.github.com>
2025-05-08 10:16:36 -0700

    Completed exercise 7

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let reversed = "";
 
+    for (n = string.length - 1; n >= 0; n--) {
+        reversed += string[n];
+    };
+
+    return reversed;
 };
 
 // Do not edit below this line
