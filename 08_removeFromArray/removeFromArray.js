702b16df1fd29775dc48017c14fcbc9b96d21ebb
sjmy <23250749+sjmy@users.noreply.github.com>
2025-05-08 12:54:14 -0700

    Completed exercise 8

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...others) {
+    let result = [];
+
+    result = array.filter((item) => !others.includes(item));
+
+    return result;
 };
 
 // Do not edit below this line
