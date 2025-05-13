ad20f29d134854b0ea0162767324b86c20471343
sjmy <23250749+sjmy@users.noreply.github.com>
2025-05-12 21:00:43 -0700

    Completed exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    fibNums = [1, 1];
 
+    if (num == 0) {
+        return 0;
+    } else if (num < 0) {
+        return "OOPS";
+    } else {
+        for (let n = 1; n < num; n++) {
+            fibNums.push(fibNums[n] + fibNums[n - 1]);
+        };
+    };
+
+    return fibNums[num - 1];
 };
 
 // Do not edit below this line
