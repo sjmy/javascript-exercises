7739801aeb84aff56bc6e7a8ce69d3ed074629b7
sjmy <23250749+sjmy@users.noreply.github.com>
2025-05-12 20:13:44 -0700

    Exercise 13 completed

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let incoming = "";
+    let reversed = "";
 
+    for (let n = 0; n < string.length; n++) {
+        if (!/[a-zA-Z0-9]/.test(string[n])) {
+            continue;
+        } else {
+            incoming += string[n]; 
+        };
+    };
+
+    for (let n = string.length - 1; n >= 0; n--) {
+        if (!/[a-zA-Z0-9]/.test(string[n])) {
+            continue;
+        } else {
+            reversed += string[n]; 
+        };
+        
+    };
+
+    return reversed.toLowerCase() == incoming.toLowerCase();
 };
+
+//console.log(palindromes("Racecar!"))
 
 // Do not edit below this line
 module.exports = palindromes;
