95280314e61c12f83c049af8b841880bb648c96c
sjmy <23250749+sjmy@users.noreply.github.com>
2025-05-12 19:04:14 -0700

    Exercise 12 complete

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  if (nums.length == 0) {
+    return 0;
+  } else {
+    return nums.reduce((result, num) => result + num);
+  };
+  
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  return nums.reduce((result, num) => result * num);
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x == 0 || x == 1) {
+    return 1;
+  } else {
+    let result = 1;
+    for (let n = 2; n <= x; n++) {
+      result *= n;
+    };
+    return result;
+  };
 };
 
 // Do not edit below this line
