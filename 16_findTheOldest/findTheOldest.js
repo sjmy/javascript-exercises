42ebf4cf2f6b329fd6eb4bdaf9a499637cdff1a8
sjmy <23250749+sjmy@users.noreply.github.com>
2025-05-12 21:37:31 -0700

    Completed exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,6 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const oldest = people.sort((a, b) => {
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = new Date().getFullYear()
+        };
 
+        if (!b.yearOfDeath) {
+            b.yearOfDeath = new Date().getFullYear()
+        };
+        
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+
+        return aAge < bAge ? 1 : -1
+    });
+
+    return oldest[0];
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
